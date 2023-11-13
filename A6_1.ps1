d0088f9f25ddca7d6ef06f74e4da13e35616de80
Waseef Ullah Akhtar <waseef437@gmail.com>
2023-11-13 19:55:34 +0100

    Primer commit

---
+++ A6_1.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$ruta = "C:\Ruta\Por\Defecto",
+    [string]$nombreArchivo
+)
+
+# Verificar si se proporcionó el nombre del archivo
+if (-not $nombreArchivo) {
+    Write-Host "Por favor, proporciona el nombre del archivo o directorio."
+    exit
+}
+
+# Combinar la ruta y el nombre del archivo para obtener la ruta completa
+$rutaCompleta = Join-Path $ruta $nombreArchivo
+
+# Verificar si el archivo o directorio existe
+if (Test-Path $rutaCompleta) {
+    Write-Host "El archivo o directorio '$nombreArchivo' se encuentra en la ruta: $rutaCompleta"
+} else {
+    Write-Host "No se encontró el archivo o directorio '$nombreArchivo' en la ruta: $rutaCompleta"
+}
---
+++ A6_2.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [string]$ruta = "C:\Ruta\Directorio\Origen",
+    [string]$rutaDestino = "C:\Ruta\Destino"
+)
+
+# Verificar si se proporcionó el directorio de origen
+if (-not $ruta) {
+    Write-Host "Por favor, proporciona el directorio de origen."
+    exit
+}
+
+# Verificar si se proporcionó la ruta de destino
+if (-not $rutaDestino) {
+    Write-Host "Por favor, proporciona la ruta de destino para la copia de seguridad."
+    exit
+}
+
+# Combinar la ruta de destino con el nombre del directorio de origen para obtener la ruta completa de destino
+$rutaCompletaDestino = Join-Path $rutaDestino (Split-Path $ruta -Leaf)
+
+# Verificar si el directorio de origen existe
+if (Test-Path $ruta) {
+    # Crear la copia de seguridad
+    Copy-Item -Path $ruta -Destination $rutaCompletaDestino -Recurse -Force
+    Write-Host "Copia de seguridad completada. Directorio de destino: $rutaCompletaDestino"
+} else {
+    Write-Host "El directorio de origen '$ruta' no existe."
+}
